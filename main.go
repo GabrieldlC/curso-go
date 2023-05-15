106fdb5182c62d2c19fdb03c2d30d5d172a32c5f
GabrieldlC <gabriel.delacuadra@outlook.com.ar>
2023-05-15 13:44:32 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/curso-go/variables"
+	"runtime"
 )
 
 func main() {
-	estado, num := variables.ConviertoATexto(618)
+	/*estado, num := variables.ConviertoATexto(618)
 
 	fmt.Println(estado)
-	fmt.Println(num)
+	fmt.Println(num)*/
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Println("Esto es", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
