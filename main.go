2e208f9c853003b6de957ad9cb8815a3272c17f2
GabrieldlC <gabriel.delacuadra@outlook.com.ar>
2023-05-15 13:22:45 -0300

    Segundo commit

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/curso-go/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, num := variables.ConviertoATexto(618)
+
+	fmt.Println(estado)
+	fmt.Println(num)
 }
