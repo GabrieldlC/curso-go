5d39561927577fd68adf75b92154b7190142cd1c
GabrieldlC <gabriel.delacuadra@outlook.com.ar>
2023-05-15 17:23:17 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/curso-go/ejercicios"
+	"github.com/curso-go/teclado"
 )
 
 func main() {
-	num, str := ejercicios.MayorMejorQueCien("20")
+	/*num, str := ejercicios.MayorMejorQueCien("20")
 	fmt.Println(num)
-	fmt.Println(str)
+	fmt.Println(str)*/
+
+	teclado.Multiplicacion()
 }
